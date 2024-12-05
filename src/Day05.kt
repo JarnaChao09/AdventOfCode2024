328649cc2d86231127a82602d004e7b5c4c6ef53
JarnaChao09 <yunyang4928@gmail.com>
2024-12-05 00:43:33 -0500

    Day 5 Solutions

---
+++ src/Day05.kt
@@ -0,0 +1,51 @@
+import kotlin.io.path.Path
+import kotlin.io.path.readText
+
+fun main() {
+    val input = Path("src/input/Day05_Test.txt").readText().trim().split("\n\n")
+
+    val (rules, updates) = input.map { it.split("\n") }
+
+    val firstToSecond = mutableMapOf<Int, List<Int>>()
+
+    for (rule in rules) {
+        val (f, s) = rule.split("|").map(String::toInt)
+
+        firstToSecond[f] = firstToSecond.getOrElse(f) { emptyList() } + s
+    }
+
+    fun isOrdered(row: List<Int>): Boolean {
+        var ordered = true
+        for ((ruleKey, ruleValues) in firstToSecond) {
+            for (ruleValue in ruleValues) {
+                if (ruleKey in row && ruleValue in row) {
+                    val ki = row.indexOf(ruleKey)
+                    val kv = row.indexOf(ruleValue)
+
+                    if (kv < ki) {
+                        ordered = false
+                    }
+                }
+            }
+        }
+
+        return ordered
+    }
+
+    val (part1, part2) = run {
+        updates.fold(0 to 0) { (f, s), it ->
+            val tmp = it.split(",").map(String::toInt)
+
+            if (isOrdered(tmp)) {
+                (f + tmp[tmp.size / 2]) to s
+            } else {
+                val stmp = tmp.sortedWith { x, y ->
+                    if (x in firstToSecond && y in firstToSecond[x]!!) -1 else 1
+                }
+                f to (s + stmp[stmp.size / 2])
+            }
+        }
+    }
+    part1.println()
+    part2.println()
+}
